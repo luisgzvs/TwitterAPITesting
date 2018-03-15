7b9dcefe74fd955caffacf9e57f89b25e5d13c0a
Luis Gonzalez <luisgzvs@gmail.com>
2018-03-15 16:37:05 -0600

    last changes

--- src/test/java/TestSetup/TestSetup.java
+++ src/test/java/TestSetup/TestSetup.java
@@ -1,36 +1,39 @@
 package TestSetup;
 
 import io.restassured.RestAssured;
-import org.testng.annotations.*;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
 import static io.restassured.RestAssured.given;
 
 
 public class TestSetup {
 
     @BeforeTest
-    public static void setup() {
-        //si es local se usa esto:
-        /*
-        String port = System.getProperty("server.port");
-        if (port == null) {
-            RestAssured.port = Integer.valueOf(3000);
+    @Parameters({"ExecutionType"})
+    public static void setup(String ExcutionType) {
+        if (ExcutionType.equals("local")) {
+            String port = System.getProperty("server.port");
+            if (port == null) {
+                RestAssured.port = Integer.valueOf(3000);
+            } else {
+                RestAssured.port = Integer.valueOf(port);
+            }
         } else {
-            RestAssured.port = Integer.valueOf(port);
-        }*/
+            String basePath = System.getProperty("server.base");
+            if (basePath == null) {
+                basePath = "/";
+            }
+            RestAssured.basePath = basePath;
 
-        String basePath = System.getProperty("server.base");
-        if (basePath == null) {
-            basePath = "/";
+            String baseHost = System.getProperty("server.host");
+            if (baseHost == null) {
+                baseHost = "https://twitter.com/";
+            }
+            RestAssured.baseURI = baseHost;
         }
-        RestAssured.basePath = basePath;
-
-        String baseHost = System.getProperty("server.host");
-        if (baseHost == null) {
-            baseHost = "https://twitter.com/";
-        }
-        RestAssured.baseURI = baseHost;
     }
-
 
     @Test
     public void confirmation() {
